34abfbbaed440495b0f1679c09983a1eb2118af2
mattdickers <mattdickers@gmail.com>
2021-05-22 17:03:58 +0100

    Integrated Project Hydra

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import random
 import time
 import datetime
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,8 +13,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.all()
 intents.members = True
-prefix = '*'
-paid = False
+prefix = '#'
 Name = 'mattbaddlesmere'
 startDate = datetime.datetime(2020,10,15,18).timestamp()  #1602781200 #Unix timestamp for 15/10/2020 at 18:00:00
 
@@ -59,21 +59,42 @@ async def paid(ctx):
 @client.event
 async def on_member_update(before, after):
     if (after.name == Name) and (after.status is discord.Status.online):
-        print('Gavin Online')
-        if random.randint(1, 20) > 10:
-            message = messages()
-            print('Sending Message:', message)
-            await client.get_channel(channels[random.randint(0,len(channels)-1)].id).send(message)
+        print(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'), 'Gavin Online')
+        roll = random.randint(1, 20)
+        print('Roll:', roll)
+        if roll < 5:
+            await send_Message()
         print()
 
-@tasks.loop(seconds=1.0)
+@tasks.loop(hours=6.0)
 async def checkTimeOfDay():
-    print('Time Trigger')
-    if random.randint(1,20) > 0:
-        message = messages()
-        print('Sending Message:', message)
-        await client.get_channel(channels[random.randint(0,len(channels)-1)].id).send(message)
+    print(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'), 'Time Trigger')
+    roll = random.randint(1, 20)
+    print('Roll:', roll)
+    if roll >= 10:
+        await send_Message()
     print()
+
+@client.event
+async def on_raw_message_delete(payload):
+    channel = payload.channel_id
+    await asyncio.sleep(5)
+    async for entry in guild.audit_logs(limit=1, action=discord.AuditLogAction.message_delete):
+        if str(entry.target) == '£100Bot#5599':
+            if str(entry.user) != 'mattbaddlesmere#0426':
+                if (datetime.datetime.utcnow()-entry.created_at).total_seconds() < 300:
+                    print(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'), 'Message Deleted by',str(entry.user))
+                    await send_Message(channel)
+                    await send_Message(channel)
+                print()
+
+async def send_Message(channel=None):
+    message = messages()
+    print('Sending Message:', message)
+    if channel:
+        await client.get_channel(channel).send(message)
+    else:
+        await client.get_channel(channels[random.randint(0, len(channels) - 1)].id).send(message)
 
 def messages():
     options = [
@@ -97,6 +118,7 @@ def messages():
         'In 1950, £100 would be worth £3000 today, you are lucky we are in 2021 ' + str(Gavin.mention),
         'The average male in the UK lives to 79, so if ' + str(Gavin.mention) + ' paid Matt £' + str(round(100/(79-22),2)) + ' a year he would fully pay back £100 before he dies',
         'A good quality pigeon is worth £20-£30. Matt could have 3-5 pigeons if ' + str(Gavin.mention) + ' paid him £100'
+        'Matt really hopes that ' + str(Gavin.mention) + 'will be paying him his £100 as soon as he has it, like he said he would and not on other things...'
     ]
     return options[random.randint(0,len(options)-1)]
 
